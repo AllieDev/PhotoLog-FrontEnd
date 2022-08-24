e697dc42ef46e42e29886db1aff40bbaef9a3913
Elena J <sjoo8503@gmail.com>
2022-08-24 13:29:38 +1200

    create component for home page

---
+++ src/components/PhotoLogHomeItem.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="component">
+    <img class="component__image" src="../assets/img/fox.jpg" alt="fox">
+    <div class="component__details">
+      <div class="component__title">
+        <p>Fox</p>
+      </div>
+      <div class="component__author">
+        <p>Author: Ray Hennessy</p>
+      </div>
+      <div class="component__source">
+        <p>Unsplash.com</p>
+        <a>https://unsplash.com/photos/xUUZcpQlqpM</a>
+      </div>
+    </div>
+  </div>
+  <div class="component">
+    <img class="component__image" src="../assets/img/fox.jpg" alt="fox">
+    <div class="component__details">
+      <div class="component__title">
+        <p>Fox</p>
+      </div>
+      <div class="component__author">
+        <p>Author: Ray Hennessy</p>
+      </div>
+      <div class="component__source">
+        <p>Unsplash.com</p>
+        <a>https://unsplash.com/photos/xUUZcpQlqpM</a>
+      </div>
+    </div>
+  </div>
+  <div class="component">
+    <img class="component__image" src="../assets/img/fox.jpg" alt="fox">
+    <div class="component__details">
+      <div class="component__title">
+        <p>Fox</p>
+      </div>
+      <div class="component__author">
+        <p>Author: Ray Hennessy</p>
+      </div>
+      <div class="component__source">
+        <p>Unsplash.com</p>
+        <a>https://unsplash.com/photos/xUUZcpQlqpM</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style scoped>
+.component {
+  display: flex;
+  font-size: 2rem;
+  border: solid 2px #000;
+  border-radius: 10px;
+  padding: 2rem;
+  margin: 5rem auto 0;
+  max-width: 1150px;
+  transition: 0.5s;
+  transform: scale(1);
+}
+
+.component:hover {
+  transform: scale(1.05);
+  opacity: 1;
+}
+
+.component__image {
+  width: 45%;
+  border-radius: 10px;
+}
+
+.component__details {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  gap: 7rem;
+  margin: auto;
+  padding: 2rem;
+  height: 40%;
+}
+
+.component__title {
+  font-size: 5rem;
+  margin: auto;
+}
+
+.component__source {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,26 @@
 <template>
-  <div>
-    <h1>Home View</h1>
+  <div class="home-component">
+    <photo-log-home-item />
   </div>
 </template>
 
 <script>
+import PhotoLogHomeItem from "../components/PhotoLogHomeItem.vue";
 
 export default {
+  data() {
+    return {
 
+    }
+  },
+  components: {
+    PhotoLogHomeItem,
+  },
 }
 </script>
 
 <style scoped>
+
 </style>
 
 
